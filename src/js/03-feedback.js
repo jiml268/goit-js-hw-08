be856a6ff8bcdc068ea5dd19991efc1806cc8c96
James Lynch <jimlynch268@gmail.com>
2023-02-14 09:58:38 -0500

    correction to part 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,20 +18,13 @@ function onFormInput(event) {
   let inputData = localStorage.getItem(LOCALSTORAGE_KEY);
   inputData = inputData ? JSON.parse(inputData) : {};
   inputData[event.target.name] = event.target.value;
-  console.log(event.target.name)
-  console.log(event.target.value)
-
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
 }
 
 function initForm() {
   let fillForm = localStorage.getItem(LOCALSTORAGE_KEY);
-  console.log(fillForm)
-
   if (fillForm) {
     fillForm = JSON.parse(fillForm);
-    console.log(fillForm)
-
     Object.entries(fillForm).forEach(([name, value]) => {
         formInfo.elements[name].value = value;
     });
