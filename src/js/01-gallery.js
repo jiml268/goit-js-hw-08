24aec804b8b97005ab26f240b781fe7efeb70b12
James Lynch <jimlynch268@gmail.com>
2023-02-14 12:30:59 -0500

    corrections after homework review

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
+import { galleryItems } from './gallery-items';
+// Add imports above this line
+// Change code below this line
 let pictureGallary = "";
 
 const galleryClass = document.querySelector(".gallery");
 galleryClass.addEventListener("click", selectimage);
 
 function createGallary(){
-    const creatPictureGallary = galleryItems.map((element) =>{
+    galleryItems.map((element) =>{
       pictureGallary += `<a class="gallery__item" href="${element.original}">
   <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
 </a>`
     }).join('')
-
-    const allPictures = document.querySelector('.gallery');
-    allPictures.insertAdjacentHTML("afterbegin",pictureGallary);
+    galleryClass.insertAdjacentHTML("afterbegin",pictureGallary);
     }
 
     function selectimage(event){
@@ -29,6 +26,4 @@ function createGallary(){
         lightbox.on('show.simplelightbox', function () {})        
     }
 }
-
-
     createGallary()
